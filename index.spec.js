3c08d3577331e3aa99c1bfdef60976a9c285960e
Joe Barnett <joe.barnett@elliptic.co>
2024-11-10 15:01:04 +0000

    add passing version of the test

--- index.spec.js
+++ index.spec.js
@@ -2,7 +2,17 @@ function delay(ms) {
   return new Promise((res) => setTimeout(res, ms));
 }
 
-it("should call callback", async () => {
+it("this passes", async () => {
+  jest.useFakeTimers({
+    doNotFake: ["setTimeout"],
+  });
+  const cb = jest.fn();
+  setTimeout(cb, 0);
+  await delay(1000);
+  expect(cb).toHaveBeenCalled();
+});
+
+it("this should also pass, but it fails", async () => {
   jest.useFakeTimers({
     doNotFake: [
       "Date",
